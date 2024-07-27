9a5685017f383cc877cf8ff85977ed1e8067ed39
AdnanAjredini1 <adnanajredini144@gmail.com>
2024-07-27 19:34:03 +0200

    W

--- src/components/ModalItems.jsx
+++ src/components/ModalItems.jsx
@@ -1,10 +1,10 @@
 export default function ModalItems({text, colorNum, color }) {
     return (
-        <div className="modal-item" style={{display:'flex',flexDirection:'row',flexWrap:'wrap', justifyContent:'space-between'}}>
+        <div className="modal-item" style={{display:'flex',flexDirection:'row',flexWrap:'wrap', justifyContent:'space-between',paddingRight:'0.4rem'}}>
             <p>{text}</p>
-            <div>
+            <div style={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:'0.7rem'}}>
                 <p>{colorNum}</p>
-                <div style={{color:color}}></div>
+                <div style={{backgroundColor:color}} className="items-color"></div>
             </div>
         </div>
     );
